e14a7f8d06bf8303d0ffdb4f640e3ff3d2162ece
Stacy Crochet <sac7850@msn.com>
2012-11-02 15:18:15 -0400

    added new files

---
+++ myrubysite/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrubysite::Application.config.secret_token = 'ee7fe38310c4c610abd93a61d54c90aa4a4e25f2c7dae5eb9453928f42703cff88af871c6018f5379b901daeb635da91bc15da16051cabf122d39de2951ad41f'
